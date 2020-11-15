5d046b85e6fd9164245a65e51d074bcb672df1c9
komod0 <juliancrespo15@gmail.com>
2020-11-15 19:13:50 -0300

    Use env vars from heroku

--- main.py
+++ main.py
@@ -6,13 +6,24 @@ from firebase_admin import credentials, initialize_app, storage
 
 app = FastAPI()
 
-client_secret_raw = os.environ["FIREBASE-JSON-CERT"].replace('\\n', '\n')
-print(fr"{client_secret_raw}")
-client_secret = json.loads(client_secret_raw)
 storage_bucket = os.environ.get("FIREBASE-STORAGE-BUCKET")
 
 # Init firebase with your credentials
-cred = credentials.Certificate(client_secret)
+cred = credentials.Certificate(
+{
+  "type": "service_account",
+  "project_id": os.environ.get("FIREBASE-PROJECT-ID"),
+  "private_key_id": os.environ.get("FIREBASE-PRIVATE-KEY-ID"),
+  "private_key": os.environ.get("FIREBASE-PRIVATE-KEY").replace('\\n', '\n'),
+  "client_email": os.environ.get("FIREBASE-CLIENT-EMAIL"),
+  "client_id": os.environ.get("FIREBASE-CLIENT-ID"),
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": os.environ.get("FIREBASE-CLIENT-CERT-URL")
+}
+    )
+
 initialize_app(cred, {'storageBucket': storage_bucket})
 
 
