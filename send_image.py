a614c9f049335e7a2638ae84e18bcfb439b175aa
julian.crespo <julian.crespo@mulesoft.com>
2021-05-17 16:39:52 -0300

    eeee

--- send_image.py
+++ send_image.py
@@ -3,4 +3,4 @@ import requests
 file_path = "/home/julianc/Wallpapers/karsten-wurth-7BjhtdogU3A-unsplash.jpg"
 files = {"file": open(file_path, "rb")}
 response = requests.post("https://bookbnb-firebase-test.herokuapp.com/upload_image", files=files)
-# aaaaaaaaa
+# aaaaeeeeaaaaa
